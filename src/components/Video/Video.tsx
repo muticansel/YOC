d32a5f80993e663a9f729eca5fe88f5d1b9a4009
cmuti <mustafa.tola@metlife.com.tr>
2021-09-30 00:23:12 +0300

    add video auto play when it is on %50 of screen

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useRef } from 'react';
 import '../../App.css';
 
 interface VideoProp {
@@ -7,8 +7,36 @@ interface VideoProp {
 
 export const Video: FunctionComponent<VideoProp> = (props) => {
     const { source } = props;
-    return <div className="video-container">
-        <video controls width="100%" height="100%">
+
+    const videoContainerRef = useRef(null);
+
+    useEffect(() => {
+        window.addEventListener('load', videoScroll);
+        window.addEventListener('scroll', videoScroll);
+    }, [])
+
+    const videoScroll = () => {
+        if (document.querySelectorAll('video[autoplay]').length > 0) {
+            const windowHeight = window.innerHeight;
+            const videoEl = document.querySelectorAll('video[autoplay]');
+
+            for (var i = 0; i < videoEl.length; i++) {
+                let thisVideoEl: any = videoEl[i]
+                // let videoHeight = thisVideoEl.clientHeight;
+                let videoClientRect = thisVideoEl.getBoundingClientRect().top;
+
+                if(videoClientRect <= windowHeight / 2) {
+                    thisVideoEl.play();
+                } else {
+                    thisVideoEl.pause();
+                }
+
+            }
+        }
+    }
+
+    return <div className="video-container" ref={videoContainerRef}>
+        <video autoPlay muted playsInline controls loop poster="http://placehold.it/350x350" width="100%" height="100%">
             <source src={source} type="video/mp4" />
         </video>
     </div>
