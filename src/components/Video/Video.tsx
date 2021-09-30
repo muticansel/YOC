2a78945fe08a1362ee3f546af1a48347dc92bc64
cmuti <mustafa.tola@metlife.com.tr>
2021-09-30 09:10:39 +0300

    fix: interval problem

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -7,6 +7,7 @@ interface VideoProp {
 
 export const Video: FunctionComponent<VideoProp> = (props) => {
     const { source } = props;
+    const [timePlayed, setTimePlayed] = useState(0);
     const [isVideoWatcherRunning, setIsVideoWatcherRunning] = useState(false);
 
     const videoContainerRef = useRef(null);
@@ -14,63 +15,68 @@ export const Video: FunctionComponent<VideoProp> = (props) => {
 
     // When we are scrolling on the page, trying to catch video is in the middle of the browser
     useEffect(() => {
-        if(!isVideoWatcherRunning) {
+        const videoScroll = () => {
+            if (document.querySelectorAll('video[autoplay]').length > 0) {
+                // GET the browser height
+                const windowHeight = window.innerHeight;
+                const videoEl = document.querySelectorAll('video[autoplay]');
+
+                for (var i = 0; i < videoEl.length; i++) {
+                    let thisVideoEl: any = videoEl[i]
+                    // GET the video borders and the top point
+                    let videoClientRect = thisVideoEl.getBoundingClientRect().top;
+
+                    if (videoClientRect > 0 && videoClientRect <= windowHeight / 2) {
+                        if (!isVideoWatcherRunning) {
+                            setIsVideoWatcherRunning(true);
+                            thisVideoEl.play();
+                        }
+                    } else {
+                        setIsVideoWatcherRunning(false);
+                        thisVideoEl.pause();
+                        stopVideoInterval();
+                    }
+
+                }
+            }
+        }
+
+        const checkVideo = () => {
+            const video: any = document.querySelectorAll('video[autoplay]')[0];
+
+            if (isVideoWatcherRunning && !video.paused) {
+                videoIntervalId.current = setInterval(() => {
+                    // If video is playing, write console.log
+                    if (!video.paused) {
+                        let ratio = video.currentTime / video.duration;
+                        if (ratio > 0.25 && ratio < 0.5) {
+                            console.log("PLAYED: %25");
+                        } else if (ratio > 0.5 && ratio < 0.75) {
+                            console.log("PLAYED: %50");
+                        } else if (ratio > 0.75 && ratio < 0.99) {
+                            console.log("PLAYED: %75");
+                        } if (ratio > 0.99) {
+                            console.log("PLAYED: %100");
+                        }
+                    }
+                }, 1000);
+            }
+        }
+
+        // Check the video watcher
+        // If it is running, check the video playing ratio
+        if (!isVideoWatcherRunning) {
             window.addEventListener("load", videoScroll);
             window.addEventListener('scroll', videoScroll);
+        } else {
+            checkVideo()
         }
     }, [isVideoWatcherRunning])
-
-    const videoScroll = async () => {
-        if (document.querySelectorAll('video[autoplay]').length > 0) {
-            // GET the browser height
-            const windowHeight = window.innerHeight;
-            const videoEl = document.querySelectorAll('video[autoplay]');
-
-            for (var i = 0; i < videoEl.length; i++) {
-                let thisVideoEl: any = videoEl[i]
-                // GET the video borders and the top point
-                let videoClientRect = thisVideoEl.getBoundingClientRect().top;
-
-                if (videoClientRect > 0 && videoClientRect <= windowHeight / 2) {
-                    if(!isVideoWatcherRunning) {
-                        await setIsVideoWatcherRunning(true);
-                        thisVideoEl.play();
-                        checkVideo();
-                    }
-                } else {
-                    setIsVideoWatcherRunning(false);
-                    thisVideoEl.pause();
-                    stopVideoInterval();
-                }
-
-            }
-        }
-    }
-
-    const checkVideo = () => {
-        videoIntervalId.current = setInterval(() => {
-            const video: any = document.querySelectorAll('video[autoplay]')[0];
-            const ratio = video.currentTime / video.duration;
-
-            // If video is playing, write console.log
-            if(!video.paused) {
-                if (ratio > 0.25 && ratio < 0.5) {
-                    console.log("PLAYED: %25");
-                } else if(ratio > 0.5 && ratio < 0.75) {
-                    console.log("PLAYED: %50");
-                } else if(ratio > 0.75 && ratio < 0.99) {
-                    console.log("PLAYED: %75");
-                } if(ratio > 0.99) {
-                    console.log("PLAYED: %100");
-                }
-            }
-        }, 1000);
-    }
 
     // Clears the interval
     const stopVideoInterval = () => {
         setIsVideoWatcherRunning(false);
-        clearInterval(videoIntervalId);
+        clearInterval(videoIntervalId.current);
     }
 
     return <div className="video-container" ref={videoContainerRef}>
